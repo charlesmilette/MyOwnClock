f6cbbbcea41187a1e341bae6ecb5d087af6c35e3
Charles Milette <charles.milette@gmail.com>
2017-07-16 15:12:52 -0400

    Make use of helper

--- Views/MainWindow.xaml.cs
+++ Views/MainWindow.xaml.cs
@@ -24,66 +24,9 @@ namespace MyOwnClock.Views
             InitializeComponent();
         }
 
-        [DllImport("user32.dll")]
-        internal static extern int SetWindowCompositionAttribute(IntPtr hwnd, ref WindowCompositionAttributeData data);
-
-        [StructLayout(LayoutKind.Sequential)]
-        internal struct WindowCompositionAttributeData
-        {
-            public WindowCompositionAttribute Attribute;
-            public IntPtr Data;
-            public int SizeOfData;
-        }
-
-        internal enum WindowCompositionAttribute
-        {
-            // ...
-            WCA_ACCENT_POLICY = 19
-            // ...
-        }
-
-        internal enum AccentState
-        {
-            ACCENT_DISABLED = 0,
-            ACCENT_ENABLE_GRADIENT = 1,
-            ACCENT_ENABLE_TRANSPARENTGRADIENT = 2,
-            ACCENT_ENABLE_BLURBEHIND = 3,
-            ACCENT_INVALID_STATE = 4
-        }
-
-        [StructLayout(LayoutKind.Sequential)]
-        internal struct AccentPolicy
-        {
-            public AccentState AccentState;
-            public int AccentFlags;
-            public int GradientColor;
-            public int AnimationId;
-        }
-
-        internal void EnableBlur(IntPtr hWnd)
-        {
-            var accent = new AccentPolicy();
-            var accentStructSize = Marshal.SizeOf(accent);
-            accent.AccentState = AccentState.ACCENT_ENABLE_BLURBEHIND;
-
-            var accentPtr = Marshal.AllocHGlobal(accentStructSize);
-            Marshal.StructureToPtr(accent, accentPtr, false);
-
-            var data = new WindowCompositionAttributeData();
-            data.Attribute = WindowCompositionAttribute.WCA_ACCENT_POLICY;
-            data.SizeOfData = accentStructSize;
-            data.Data = accentPtr;
-
-            SetWindowCompositionAttribute(hWnd, ref data);
-
-            Marshal.FreeHGlobal(accentPtr);
-        }
-
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            var hWnd = new WindowInteropHelper(this).Handle;
-
-            EnableBlur(hWnd);
+            new WindowInteropHelper(this).SetWindowCompositionAttribute();
         }
 
         protected override void OnMouseLeftButtonDown(MouseButtonEventArgs e)
