07c3dabdc158e3301f7b365091f1ce981123b03d
Charles Milette <charles.milette@gmail.com>
2017-12-01 15:19:55 -0500

    Use a stackpanel for the window controls

--- Extensions/CustomProperties.cs
+++ Extensions/CustomProperties.cs
@@ -5,20 +5,12 @@ namespace MyOwnClock.Extensions
 {
     public class CustomProperties
     {
-        public static readonly DependencyProperty MouseOverColorProperty =
-
-    DependencyProperty.RegisterAttached("MouseOverColor", typeof(Brush), typeof(CustomProperties), new PropertyMetadata(default(Brush)));
-
+        public static readonly DependencyProperty MouseOverColorProperty = DependencyProperty.RegisterAttached("MouseOverColor", typeof(Brush), typeof(CustomProperties), new PropertyMetadata(default(Brush)));
         public static void SetMouseOverColor(UIElement element, Brush value) => element.SetValue(MouseOverColorProperty, value);
-
         public static Brush GetMouseOverColor(UIElement element) => (Brush)element.GetValue(MouseOverColorProperty);
 
-        public static readonly DependencyProperty PressedColorProperty =
-
-DependencyProperty.RegisterAttached("PressedColor", typeof(Brush), typeof(CustomProperties), new PropertyMetadata(default(Brush)));
-
+        public static readonly DependencyProperty PressedColorProperty = DependencyProperty.RegisterAttached("PressedColor", typeof(Brush), typeof(CustomProperties), new PropertyMetadata(default(Brush)));
         public static void SetPressedColor(UIElement element, Brush value) => element.SetValue(PressedColorProperty, value);
-
         public static Brush GetPressedColor(UIElement element) => (Brush)element.GetValue(PressedColorProperty);
     }
 }
