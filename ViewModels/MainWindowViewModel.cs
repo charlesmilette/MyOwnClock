3469fc65298470d7a3e25f92f18f4455df99a49f
Charles Milette <charles.milette@gmail.com>
2017-12-01 16:49:26 -0500

    Move things around in viewmodel

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -6,107 +6,62 @@ namespace MyOwnClock.ViewModels
 {
     public class MainWindowViewModel : ViewModel
     {
-        /* コマンド、プロパティの定義にはそれぞれ 
-         * 
-         *  lvcom   : ViewModelCommand
-         *  lvcomn  : ViewModelCommand(CanExecute無)
-         *  llcom   : ListenerCommand(パラメータ有のコマンド)
-         *  llcomn  : ListenerCommand(パラメータ有のコマンド・CanExecute無)
-         *  lprop   : 変更通知プロパティ(.NET4.5ではlpropn)
-         *  
-         * を使用してください。
-         * 
-         * Modelが十分にリッチであるならコマンドにこだわる必要はありません。
-         * View側のコードビハインドを使用しないMVVMパターンの実装を行う場合でも、ViewModelにメソッドを定義し、
-         * LivetCallMethodActionなどから直接メソッドを呼び出してください。
-         * 
-         * ViewModelのコマンドを呼び出せるLivetのすべてのビヘイビア・トリガー・アクションは
-         * 同様に直接ViewModelのメソッドを呼び出し可能です。
-         */
-
-        /* ViewModelからViewを操作したい場合は、View側のコードビハインド無で処理を行いたい場合は
-         * Messengerプロパティからメッセージ(各種InteractionMessage)を発信する事を検討してください。
-         */
-
-        /* Modelからの変更通知などの各種イベントを受け取る場合は、PropertyChangedEventListenerや
-         * CollectionChangedEventListenerを使うと便利です。各種ListenerはViewModelに定義されている
-         * CompositeDisposableプロパティ(LivetCompositeDisposable型)に格納しておく事でイベント解放を容易に行えます。
-         * 
-         * ReactiveExtensionsなどを併用する場合は、ReactiveExtensionsのCompositeDisposableを
-         * ViewModelのCompositeDisposableプロパティに格納しておくのを推奨します。
-         * 
-         * LivetのWindowテンプレートではViewのウィンドウが閉じる際にDataContextDisposeActionが動作するようになっており、
-         * ViewModelのDisposeが呼ばれCompositeDisposableプロパティに格納されたすべてのIDisposable型のインスタンスが解放されます。
-         * 
-         * ViewModelを使いまわしたい時などは、ViewからDataContextDisposeActionを取り除くか、発動のタイミングをずらす事で対応可能です。
-         */
-
-        /* UIDispatcherを操作する場合は、DispatcherHelperのメソッドを操作してください。
-         * UIDispatcher自体はApp.xaml.csでインスタンスを確保してあります。
-         * 
-         * LivetのViewModelではプロパティ変更通知(RaisePropertyChanged)やDispatcherCollectionを使ったコレクション変更通知は
-         * 自動的にUIDispatcher上での通知に変換されます。変更通知に際してUIDispatcherを操作する必要はありません。
-         */
-
-        DispatcherTimer dispatcherTime = new DispatcherTimer(DispatcherPriority.Normal);
-
         public void Initialize()
         {
-            dispatcherTime.Interval = new TimeSpan(0, 0, 1);
-            dispatcherTime.Tick += TimerTick;
-            dispatcherTime.Start();
+            new DispatcherTimer(
+                TimeSpan.FromSeconds(1),
+                DispatcherPriority.Normal,
+                (object sender, EventArgs e) =>
+                {
+                    DateTime now = DateTime.Now;
+                    TimeString = now.ToShortTimeString();
+                    SecondString = now.Second.ToString("00");
+                    DateString = now.ToLongDateString();
+                },
+                Dispatcher.CurrentDispatcher
+            ).Start();
         }
 
-        private void TimerTick(object sender, EventArgs e)
+        private string _TimeString;
+        public string TimeString
         {
-            DateTime now = DateTime.Now;
-            TimeString = now.ToString("HH:mm");
-            SecondString = now.ToString("ss");
-            DateString = now.ToString("yyyy/MM/dd dddd");
-        }
-
-
-        #region TimeString変更通知プロパティ
-        private string _TimeString = DateTime.Now.ToString("HH:mm");
-
-        public string TimeString {
             get => _TimeString;
-            set { 
-                if (_TimeString == value)
-                    return;
-                _TimeString = value;
-                RaisePropertyChanged();
+            set
+            {
+                if (_TimeString != value)
+                {
+                    _TimeString = value;
+                    RaisePropertyChanged();
+                }
             }
         }
-        #endregion
 
-        #region SecondString変更通知プロパティ
-        private string _SecondString = DateTime.Now.ToString("ss");
-
-        public string SecondString {
+        private string _SecondString;
+        public string SecondString
+        {
             get => _SecondString;
-            set { 
-                if (_SecondString == value)
-                    return;
-                _SecondString = value;
-                RaisePropertyChanged();
+            set
+            {
+                if (_SecondString != value)
+                {
+                    _SecondString = value;
+                    RaisePropertyChanged();
+                }
             }
         }
-        #endregion
 
-        #region DateString変更通知プロパティ
-        private string _DateString = DateTime.Now.ToString("yyyy/MM/dd dddd");
-
-        public string DateString {
+        private string _DateString;
+        public string DateString
+        {
             get => _DateString;
-            set { 
-                if (_DateString == value)
-                    return;
-                _DateString = value;
-                RaisePropertyChanged();
+            set
+            {
+                if (_DateString != value)
+                {
+                    _DateString = value;
+                    RaisePropertyChanged();
+                }
             }
         }
-        #endregion
-
     }
 }
