d55a0fc84012d791dfaf86114a12c87f32517742
Charles Milette <charles.milette@gmail.com>
2017-12-01 13:50:14 -0500

    Add missing file

---
+++ Extensions/CustomProperties.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+using System.Windows.Media;
+
+namespace MyOwnClock.Extensions
+{
+    public class CustomProperties
+    {
+        public static readonly DependencyProperty MouseOverColorProperty =
+
+    DependencyProperty.RegisterAttached("MouseOverColor", typeof(Brush), typeof(CustomProperties), new PropertyMetadata(default(Brush)));
+
+        public static void SetMouseOverColor(UIElement element, Brush value) => element.SetValue(MouseOverColorProperty, value);
+
+        public static Brush GetMouseOverColor(UIElement element) => (Brush)element.GetValue(MouseOverColorProperty);
+
+        public static readonly DependencyProperty PressedColorProperty =
+
+DependencyProperty.RegisterAttached("PressedColor", typeof(Brush), typeof(CustomProperties), new PropertyMetadata(default(Brush)));
+
+        public static void SetPressedColor(UIElement element, Brush value) => element.SetValue(PressedColorProperty, value);
+
+        public static Brush GetPressedColor(UIElement element) => (Brush)element.GetValue(PressedColorProperty);
+    }
+}
